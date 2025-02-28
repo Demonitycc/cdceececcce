aa50699891e0904617509fe8d429a9d7ff4adab3
Demonitycc <lolsavvy418@gmail.com>
2025-02-27 22:19:53 -0500

    Create silent.lua

---
+++ silent.lua
@@ -0,0 +1,21 @@
+local saim; saim = hookfunction(unpack, newcclosure(function(...) 
+  local args = {...}
+  local stack = debug.getstack(1, 1)
+  if typeof(stack[1][9]) == "CFrame" and getgenv().SilentEnabled and getgenv().Target and getgenv().Target.Head then
+      local targetHead = getgenv().Target.Head
+      --
+      if not targetHead then
+          return saim(...)
+      end
+      --
+      local Camera = game.workspace.CurrentCamera
+      local cameraPosition = Camera.CFrame.Position
+      local predictedPos = getgenv().Predict(targetHead.Position, getgenv().PlayersVelocity or Vector3.new(0, 0, 0))
+      if predictedPos then
+          stack[1][9] = CFrame.lookAt(cameraPosition, predictedPos)
+      end
+  else
+      return saim(...)
+  end
+  return saim(...)
+end))
