c81b7cad2f24ae68125493ac1d144e572c1dbc6e
Demonitycc <lolsavvy418@gmail.com>
2025-02-27 22:50:17 -0500

    Update jumpshoot.lua

--- jumpshoot.lua
+++ jumpshoot.lua
@@ -1,8 +1,31 @@
-local oldjump; oldjumop = Newhookfunction(Modules2.Character.IsGrounded, function(...)
-    if getgenv().novas.jumpshoot then
-        return true
-    else
-        return oldjumop(...)
-    end
-    return oldjumop(...)
-end)
+local recoilhook; recoilhook = Newhookfunction(Modules2.Camera.Recoil, function(...)
+                local args = {...}
+                if getgenv().novas.Recoilend then
+                    local RecoilAmt = 0 / 100
+                    args[1].Push = 0
+                    args[1].CameraY = RecoilAmt
+                    args[1].CameraX = RecoilAmt
+                    args[1].CameraXShake = 0
+                else
+                    return recoilhook(...)
+                end
+                return recoilhook(...)
+            end)
+            --
+            local oldjump; oldjumop = Newhookfunction(Modules2.Character.IsGrounded, function(...)
+                if getgenv().novas.jumpshoot then
+                    return true
+                else
+                    return oldjumop(...)
+                end
+                return oldjumop(...)
+            end)
+            --
+            local oldsway; oldsway = Newhookfunction(Modules2.Camera.SetSwaySpeed, function(...)
+                if getgenv().novas.NoSway then
+                    return true
+                else
+                    return oldsway(...)
+                end
+                return oldsway(...)
+            end)
